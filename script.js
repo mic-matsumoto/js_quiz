f6d4122e8aff7d319493f37c4c05f5c84197df8b
Satoshi Matsumoto (mic-p) <matsumoto.satoshi@mic-p.com>
2024-06-28 15:45:21 +0900

    second commit

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ function blockText() {
     words.forEach(word => {
         const block = document.createElement('div');
         block.className = 'block';
+
         if (/^[^\w\s]+$/.test(word)) {
             block.classList.add('symbol');
+        } else if (/^[^a-zA-Z]+$/.test(word)) {
+            block.classList.add('non-alphabetic');
         }
+
         block.textContent = word;
         output.appendChild(block);
     });
